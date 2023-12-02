b30371646791a089678e0434f45fc39cb6a2adbf
Ninety <68526679+HeyNinety@users.noreply.github.com>
2023-12-02 08:53:46 -0600

    Various fixes
    Added .godot folder to .gitignore
    
    Player now uses _physics_process instead of _process
    
    PickupItem changed from RigidBody2D to AnimatableBody2D
    
    Refactored player movement. Normalized direction vector.
    
    Made node paths @onready var(s) to clean up the code a bit.

--- scripts/PickupItem.gd
+++ scripts/PickupItem.gd
@@ -1,23 +1,25 @@
-extends RigidBody2D
+extends AnimatableBody2D
 
 
-var is_held = false
+var is_held: bool = false
+@onready var player = get_node("../Player")
+@onready var itemAnchor = get_node("../Player/ItemHolder/ItemAnchor")
 
 func _physics_process(_delta):
 	if is_held == true:
 		#print("is holding!")
-		self.position = get_node("../Player/ItemHolder/ItemAnchor").global_position
+		self.position = itemAnchor.global_position
 
 func _input(_event):
 	if Input.is_action_just_pressed("pickup_item"):
 		#print("pickup item")
 		var bodies = $Area2D.get_overlapping_bodies()
 		for body in bodies:
-			if body.name == "Player" and get_node("../Player").canHold == true:
+			if body.name == "Player" and player.canHold == true:
 				is_held = true
-				get_node("../Player").canHold = false
+				player.canHold = false
 				
 	if Input.is_action_just_pressed("drop_item") and is_held == true:
 		#print("drop item")
 		is_held = false
-		get_node("../Player").canHold = true
+		player.canHold = true
