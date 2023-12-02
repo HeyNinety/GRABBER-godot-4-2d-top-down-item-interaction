7615b8559abdeffe3b983057cea4ca5f17a6f0d7
Ninety <68526679+HeyNinety@users.noreply.github.com>
2023-12-02 09:23:07 -0600

    Refactor
    Player now detects pickupItem instead of pickupItem detecting Player.

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -1,15 +1,10 @@
 extends CanvasLayer
 
+@onready var player = get_node("../Player/ItemHolder")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	#toggle UI mode
-	if get_node("../Player/ItemHolder").use_controller == true:
+	if player.use_controller == true:
 		$ControllerUI.show()
 		$"Keyboard&MouseUI".hide()
 	else:
