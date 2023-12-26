504fae29176d1dbf4bae9e2bebcefc5c628b54d8
Ninety <68526679+HeyNinety@users.noreply.github.com>
2023-12-26 15:18:52 -0600

    Revert "Revert "Revert "File cleanup + break"""
    This reverts commit d86b29c2374d49b9b649a8984cf1b5f7d3425db7.

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -1,13 +1,12 @@
 extends CanvasLayer
 
-#change this to recieve signal
+@onready var player = get_node("../Player/ItemHolder")
 
 func _process(_delta):
 	#toggle UI mode
-	#if player.use_controller == true:
-		#$ControllerUI.show()
-		#$"Keyboard&MouseUI".hide()
-	#else:
-		#$ControllerUI.hide()
-		#$"Keyboard&MouseUI".show()
-		pass
+	if player.use_controller == true:
+		$ControllerUI.show()
+		$"Keyboard&MouseUI".hide()
+	else:
+		$ControllerUI.hide()
+		$"Keyboard&MouseUI".show()
